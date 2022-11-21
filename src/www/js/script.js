f5bc41748e72e0a79ce8dcb84438fe7e85efda23
Martin Šíl <sil@spstrutnov.cz>
2022-11-21 14:10:06 +0100

    Styl kalkulačky

---
+++ src/www/js/script.js
@@ -0,0 +1,8 @@
+const procento = document.getElementById('procento');
+const ce = document.getElementById('ce');
+const c = document.getElementById('c');
+const smazat = document.getElementById('smazat');
+const jednanaxtou = document.getElementById('jednanaxtou');
+const nadruhou = document.getElementById('nadruhou');
+const odmocninadvou = document.getElementById('odmocninadvou');
+const deleno = document.getElementById('deleno');const  = document.getElementById('');
